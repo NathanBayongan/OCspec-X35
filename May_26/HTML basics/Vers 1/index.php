5eab2bf68bcd50f0a6e16de37e317618c9d7801b
22BayongN <NathanBayongan@Gmail.com>
2025-09-05 13:59:29 +0100

    Added in essential guides

---
+++ May_26/HTML basics/Vers 1/index.php
@@ -0,0 +1,34 @@
+<?php // This opens the php code
+
+echo "<!DOCTYPE html>"; # Declares what type of page it is
+
+
+echo "<html>";
+
+echo "<head>";
+
+echo "<title> Nathan</title>";
+
+
+echo "</head>";
+
+echo "<body>";
+
+
+echo "</h1> Nathan </h1>";
+
+echo "<p> The Start </p>";
+
+echo "<a href='page2.php'> Go to page 2. </a>";
+
+echo "<table>";
+    echo "<tr>";
+        echo "<p><td>  ___   ___</td></p>";
+        echo "<p><td>( <->   <-> )</td></p>";
+        echo "<p><td>      ᜊ    </td></p>";
+
+
+
+echo "</body>";
+
+?>6
