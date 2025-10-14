ad296ca2f87173c6de025e76a0f6c77349408747
22BayongN <NathanBayongan@Gmail.com>
2025-10-14 10:24:43 +0100

    Version 2 progression

---
+++ May_26/mini_project_04/Docs/Version 2/assets/common.php
@@ -0,0 +1,64 @@
+<?php
+function user_message()
+{
+    if (isset($_SESSION['usermessage'])) { // Checks if "usermessage" is set
+        $message = $_SESSION['usermessage'] . "</p>"; // styles "usermessage"
+        unset($_SESSION['usermessage']); // unsets to make it not exist anymore to save storage/ memory.
+        return $message; // returns message
+    } else {
+        $message = ""; // if condition isn't met, it returns blank
+        return $message;
+    }
+}
+
+function login($conn, $post){
+    try{ // try this code, catch errors
+        $sql = "SELECT * FROM patient WHERE fname = ?"; // set up sql statement
+        $stmt = $conn->prepare($sql); // prepares
+        $stmt->bindParam(1, $post['fname']); // binds the parameters to execute
+        $stmt->execute(); // runs sql code
+        $result = $stmt->fetch(PDO::FETCH_ASSOC); // Brings back results
+        $conn = null; // Breaks off connection once it is used
+
+        if($result){ // If there is a result returned
+            return $result;
+
+        } else {
+            $SESSION['usermessage'] = "User not found";
+        }
+
+    } catch (Exception $e) {
+        $SESSION['Error'] = $e->getMessage();
+        throw new Exception("User Registration error: ", $e); // throws exception
+    }
+}
+
+function reg_user($conn, $post)
+{
+    try {
+        // prepare and execute the SQL query
+        $sql = "INSERT INTO patient (fname, sname, dob, gender, password) VALUES (?, ?, ?, ?, ?)"; // prepares statement
+        $stmt = $conn->prepare($sql); // prepare to sql
+
+        $stmt->bindParam(1, $post['fname']); // bind parameters for security
+        $stmt->bindParam(2, $post['sname']);
+        $stmt->bindParam(3, $post['dob']);
+        $stmt->bindParam(4, $post['gender']);
+        // hash the password
+        $hpswd = password_hash($post['password'], PASSWORD_DEFAULT); // has the password
+        // Using in built php library using default encryption because we have nothing else built into this code base
+        // In a business environment, it's better to use PASSWORD_BCRYPTb
+        $stmt->bindParam(5, $hpswd);
+
+        $stmt->execute(); // run the query to insert
+        $conn = null; // closes connection after use
+        return true; // registration successful
+    } catch (PDOException $e) {
+        // handles database errors
+        error_log("User Reg database error: " . $e->getMessage()); // logs the errors
+        throw new Exception("User reg database error: ", $e); // throws exception for calling script
+    } catch (Exception $e) {
+        error_log("User Registration error: " . $e->getMessage()); // logs the error
+        throw new Exception("User Registration error: ", $e); // throws exception
+    }
+}
---
+++ May_26/mini_project_04/Docs/Version 2/login.php
@@ -0,0 +1,69 @@
+<?php //this opens the php code section
+session_start();
+
+require_once"assets/dbconn.php";
+require_once"assets/common.php";
+
+if (isset($_SESSION['user'])){
+    $_SESSION["usermessage"] = "You are already logged in";
+    //header("Location: index.php");
+    exit; // stops further execution
+}
+elseif ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $usr = login(dbconnect_insert(), $_POST);
+
+    if ($usr && password_verify($_POST['password'], $usr["password"])) {
+        $_SESSION["user"] = true;
+        $_SESSION["user_id"] = $usr["patient_id"];
+        $_SESSION["usermessage"] = "Success! = user successfully logged in";
+        //auditor(dbconnect_insert(), $_SESSION["user_id"], "log", "User successfully logged in");
+        //header("Location: index.php");
+        exit;
+    } else {
+        $_SESSION["usermessage"] = "Error: Login and Password do not match";
+        //header("Location: login.php");
+        exit;
+    }
+}
+
+
+echo "<!DOCTYPE html>";  // desired tag to declare what type of page it is
+
+echo "<html>";  // opening html
+echo "<head>";  // opening head
+
+echo "<title>page title</title>";  // creating title
+echo "<link rel='stylesheet' type='text/css' href='css\styles.css'>";// getting css formatting for website from external
+
+echo "</head>";
+echo "<body>"; // opening body
+
+
+echo "<div class ='container'>"; // class container to give all items a default to reduce need for styling later
+require_once "assets/topbar.php"; // presenting header
+require_once "assets/nav.php";// presenting navigation bar
+
+echo "<div class ='content'>"; // class context to give all items that give information an overall css to reduce need for styling later and standardise formatting
+echo "<form method='post' action=''>";
+echo "<input type= 'text'name ='fname' placeholder='First Name'>";
+echo "<br>";
+echo "<input type= 'text'name ='sname' placeholder='Surname'>";
+echo "<br>";
+echo "<input type= 'password'name ='password' placeholder='password'>";
+echo "<br>";
+echo "<input type= 'submit' value='login' id='submit'>";
+echo "</form>";
+
+echo '<br>';
+echo user_message();
+
+
+echo "</div>";
+
+echo "</div>";
+
+echo "</body>";
+
+echo "</html>";
+?>
+
